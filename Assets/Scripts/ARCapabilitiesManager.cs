ed3c1842affc03422d6551e16f7911489fba67ba
John <john@real-currents.com>
2025-08-25 23:30:20 -0400

    start new project in learn xr course by dilmer valecillos

--- Assets/Scripts/ARCapabilitiesManager.cs
+++ Assets/Scripts/ARCapabilitiesManager.cs
@@ -1,53 +1,62 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using Logger = DilmerGames.Core.Logger;
 
 public class ARCapabilitiesManager : MonoBehaviour
 {
     private ARSession arSession;
-
+    
     private void Awake()
     {
         arSession = FindObjectOfType<ARSession>();
         arSession.enabled = false;
     }
-
-    private void Start()
+    
+    // Start is called before first frame update
+    private void Start ()
     {
         StartCoroutine(CheckARSupport());
     }
 
-    private void OnEnable()
-    {
-        ARSession.stateChanged += ARSessionStateChanged;
-    }
-
-    private void OnDisable()
-    {
-        ARSession.stateChanged -= ARSessionStateChanged;
-    }
-
-    private void ARSessionStateChanged(ARSessionStateChangedEventArgs obj)
-    {
-        Logger.Instance.LogInfo($"AR session state changed: {obj.state}");
-    }
-
     private IEnumerator CheckARSupport()
     {
-        if (ARSession.state == ARSessionState.None || ARSession.state == ARSessionState.CheckingAvailability)
+        if (ARSession.state == ARSessionState.None
+            || ARSession.state == ARSessionState.CheckingAvailability)
         {
-            Logger.Instance.LogInfo("Checking if AR is available on this device");
+            Logger.Instance.LogInfo("Checking if AR is available...");
             yield return ARSession.CheckAvailability();
         }
 
         if (ARSession.state != ARSessionState.Unsupported)
         {
-            Logger.Instance.LogInfo("AR is supported on this device");
+            Logger.Instance.LogInfo("This device supports AR sessions");
             arSession.enabled = true;
         }
         else
         {
-            Logger.Instance.LogInfo("Disabling AR session as it is not supported");
+            Logger.Instance.LogInfo("This device does not support AR sessions");
+            arSession.enabled = false;
         }
+        
+        throw new System.NotImplementedException();
     }
+
+    private void OnEnable() 
+    {
+        ARSession.stateChanged += ARSessionStateChanged;
+    }
+    
+    private void OnDisable() 
+    {
+        ARSession.stateChanged -= ARSessionStateChanged;
+    }
+    
+    private void ARSessionStateChanged(ARSessionStateChangedEventArgs obj) 
+    {
+        Logger.Instance.LogInfo($"AR session state changed: {obj.state}");
+    }
+    
+    // Update is called once per frame; assume non-blocking (real-time)
+    private void Update() {}
 }
